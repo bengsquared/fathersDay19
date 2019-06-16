9a3762ca430c4861a62fc4922b1c8f26d54322b3
ben <bengg@mac.com>
2019-06-15 19:51:06 -0700

    Updates

---
+++ precache-manifest.28454740c12f032b38c239bc0364bcea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d324881bc694568cb7c3c96dc98440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d253235b29cb4fc66c5f",
+    "url": "/static/css/main.dbff52fc.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d253235b29cb4fc66c5f",
+    "url": "/static/js/main.dfb67129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
